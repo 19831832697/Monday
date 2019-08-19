624020e0e6b18410654e451cdb711f5ab9fc669d
fengdan <2128516529@qq.com>
2019-08-19 11:23:48 +0800

    非对称加密

--- app/Http/Controllers/login/LoginController.php
+++ app/Http/Controllers/login/LoginController.php
@@ -24,20 +24,23 @@ class LoginController extends Controller
     {
         $data = json_encode($request->input(),JSON_UNESCAPED_UNICODE);  //待加密的明文信息数据
         //对称加密
-        $method = "AES-256-CBC";
-        $key = "Admin123";
-        $options = OPENSSL_RAW_DATA;
-        $iv="12345tgvfred2346";
-        $arrInfo = openssl_encrypt($data,$method,$key,$options,$iv);
+//        $method = "AES-256-CBC";
+//        $key = "Admin123";
+//        $options = OPENSSL_RAW_DATA;
+//        $iv="12345tgvfred2346";
+//        $arrInfo = openssl_encrypt($data,$method,$key,$options,$iv);
 
         //非对称加密
+        $k=openssl_pkey_get_private('file://'.storage_path('app/key/private.pem'));
+        openssl_private_encrypt($data,$enc_data,$k);
 
         $url = "http://lumen.1809a.com/loginDo";
         $curl = curl_init();
         curl_setopt($curl,CURLOPT_URL,$url);
         curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
         curl_setopt($curl,CURLOPT_POST,1);
-        curl_setopt($curl,CURLOPT_POSTFIELDS,$arrInfo);
+//        curl_setopt($curl,CURLOPT_POSTFIELDS,$arrInfo);
+        curl_setopt($curl,CURLOPT_POSTFIELDS,$enc_data);
         curl_setopt($curl,CURLOPT_HTTPHEADER,['Content-Type:text/plain']);
         $res = curl_exec($curl);
         var_dump($res);
